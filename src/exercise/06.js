aa09c5349fe1ffe0eac31fd39785d315e0723404
Abdul <abdul.k.khan75@gmail.com>
2021-08-28 12:36:45 -0400

    finish module 6 - form

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -19,37 +19,73 @@ function UsernameForm({onSubmitUsername}) {
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
+
+  // extra credit 1 - using refs to get value of input
+  // extra credt 2 - validating input using useState and refs
+  // extra credit 3 - control the user input by lowercasing all inputs - input normalization instead of showing field error
   
+  // ec2  
+  // const [inputErr, setInputErr] = React.useState(false)
+  // ec3
+  const [inputVal, setInputVal] = React.useState('')
   const testRef = React.useRef('')
   const handleSubmit = (event) => {
-    console.log('jere', event)
     event.preventDefault()
 
-    //1
+    // Exercise
     // onSubmitUsername(event.target.elements[0].value)
 
-    // extra credit
-    console.log('testref', testRef)
+    // ec1 - using refs
     onSubmitUsername(testRef.current.value)
+
+
   }
 
+  const handleChange = (event) => {
+    const { value } = event.target
+
+    // ec2
+    // const isValid = value === value.toLowerCase()
+    // setInputErr(isValid ? false : 'Lower case letters only')
+
+    setInputVal(value.toLowerCase())
+  }
   
 
   return (
     <form onSubmit={ handleSubmit }>
-      <div>
-        <label
-            htmlFor="inputTest"
-        >
-            Username:
-        </label>
-        <input
-          type="text"
-          id="inputTest"
-          ref={ testRef }
-        />
-      </div>
-      <button type="submit">Submit</button>
+		<div>
+			<label
+				htmlFor="inputTest"
+			>
+				Username:
+			</label>
+			<input
+				type="text"
+				id="inputTest"
+				ref={ testRef }
+				onChange={(e) => handleChange(e) }
+        value={ inputVal }
+			/>
+		</div>
+    {/* ec2 */}
+		{/* {
+			inputErr && 
+				<span
+					role="alert"
+					className="error-message"
+				>
+					{ inputErr }
+				</span>
+		} */}
+      
+		<button
+			type="submit"
+      // ec2
+			// disabled={ inputErr }        
+		>
+			Submit
+		</button>
     </form>
   )
 }
